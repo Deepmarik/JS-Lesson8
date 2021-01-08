fd06eed755e1ed20a978474f90aec9d8f2adedad
Deepmarik <lukyanovateam@gmail.com>
2021-01-08 16:03:56 +0300

    task 8.1 is done

--- 8.1-1 текст вместо картинки/app.js
+++ 8.1-1 текст вместо картинки/app.js
@@ -1,41 +1,52 @@
 'use strict';
 // 1. получите все кнопки и сохраните в переменную
-let buttons=document.getElementsByTagName("button");
+const buttons = document.querySelectorAll("button");
 // 1.1 затем проитерируйтесь по кнопкам и каждой из
 // них добавьте обработчик клика - функцию handleClick
-Array.from(buttons).forEach(function(button){
-    button.addEventListener("click", function(event){handleClick(event);})
-} );
+buttons.forEach(function (button) {
+    button.addEventListener("click", function (event) { handleClick(event); })
+});
 
 /**
  * Функция обрабатывает клик по кнопке в карточке товара и попеременно вызывает
  * функции для показа или скрытия текста о товаре.
- * @param {MouseEvent} clickedButtonEvent 
- */
+ *  @param {MouseEvent} clickedButtonEvent 
+ **/
+
 function handleClick(clickedButtonEvent) {
+    const cardNode = clickedButtonEvent.target.parentNode;
+
     // 2. из объекта события получите ссылку на .product и
     // сохраните в переменную:
     // const cardNode = ;
 
     // 3. создайте литерал объекта со следующими свойствами:
-    const card = {
-        wrap: , // здесь элемент с классом .product
-        img: , // здесь картинка внутри .product
-        productName: , // здесь .productName, который внутри .product
-        button: , // здесь button, который внутри .product
-    };
 
-    // 4. получаем текст на кнопке, которая внутри .product
-    
-    
-    if () { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
-        // 4.2 если да, то передаем объект card в функцию showMoreText
+    const card = {
+        wrap: cardNode, // здесь элемент с классом .product
+        img: cardNode.querySelector("img"), // здесь картинка внутри .product
+        productName: cardNode.querySelector(".productName"), // здесь .productName, который внутри .product
+        button: cardNode.querySelector("button"), // здесь button, который внутри .product
+    };
+    const textOnButton = card.button.innerText;
+    if (textOnButton === "Подробнее") {
         showMoreText(card);
-    } else if () { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
-        // 4.4 если да, то передаем объект card в функцию hideMoreText
+    } else if (textOnButton === "Отмена") {
         hideMoreText(card);
     }
 }
+
+// 4. получаем текст на кнопке, которая внутри .product
+
+
+// if () { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
+// 4.2 если да, то передаем объект card в функцию showMoreText
+//   showMoreText(card);
+// } else if () { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
+// 4.4 если да, то передаем объект card в функцию hideMoreText
+//  hideMoreText(card);
+// }
+
 
 /**
  * Функция скрывает текст с описанием товара.
@@ -46,12 +57,15 @@ function handleClick(clickedButtonEvent) {
  * @param {HTMLButtonElement} card.button
  */
 function hideMoreText(card) {
+    card.img.style.display="block";
+    card.wrap.querySelector(".desc").remove();
+    card.button.innerText="Подробнее";
     // 5. картинке внутри .product ставим стиль display: block
-    
+
     // 5.1 внутри .product находим элемент с классом .desc и удаляем его
-    
+
     // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
-    
+
 }
 
 /**
@@ -63,12 +77,16 @@ function hideMoreText(card) {
  * @param {HTMLButtonElement} card.button 
  */
 function showMoreText(card) {
+    card.img.style.display = "none";
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc magna risus, ornare at dui ac, scelerisque ultrices odio. Vivamus malesuada.";
+    card.productName.insertAdjacentHTML("afterend", `<div class="desc">${text}</div>`);
+    card.button.innerText="Отмена";
     // 6. картинке внутри .product ставим display: none
-    
+
     // 6.1 сохраняем произвольный текст в переменную
-    
+
     // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
-    
+
     // 6.3 внутри .product у кнопки текст ставим "Отмена"
-    
+
 }
